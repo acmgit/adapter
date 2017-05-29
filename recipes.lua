fb0bf9d2458c0e14b0ac4b1e231a10529a0e2355
acmgit <undertakers_help@yahoo.com>
2017-05-29 17:38:27 +0200

    Tinconverter added.

--- recipes.lua
+++ recipes.lua
@@ -24,7 +24,37 @@ recipelist =
 		type = "shapeless",
 		output = "food_sweet:blueberry",
 		recipe = {"bushes:blueberry"}
+	   },
+
+	   {
+		type = "shapeless",
+		output = "mesecons_luacontroller:luacontroller0000",
+		recipe = {"mesecons_microcontroller:microcontroller0000"}
+	   },
+	   
+	   {
+		type = "shapeless",
+		output = "mesecons_microcontroller:microcontroller0000",
+		recipe = {"mesecons_luacontroller:luacontroller0000"}
+	   },
+
+	   {
+		type = "shapeless",
+		output = "moreores:tin_lump",
+		recipe = {"default:tin_lump"}
+	   },
+	   
+	   {
+		type = "shapeless",
+		output = "moreores:tin_ingot",
+		recipe = {"default:tin_ingot"}
+	   },
+
+	   {
+		type = "shapeless",
+		output = "moreores:tin_block",
+		recipe = {"default:tin_block"}
 	   }
 
-	   
+
 	} -- recipelist
