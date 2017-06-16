bc922cd5c98eac963ad55ca0a8cde882f48cbbed
= <undertakers_help@yahoo.com>
2017-06-16 14:07:36 +0200

    Modcheck added.

--- init.lua
+++ init.lua
@@ -2,7 +2,15 @@ dofile(minetest.get_modpath("adapter") .. "/recipes.lua")
 
 for i,n in ipairs(recipelist) do
    
-    -- Register the Recipes
-    minetest.register_craft(n)
 
-end
+	if(minetest.get_modpath(n.mod)) then	
+	    local rec = {	type = n.type,
+				output = n.output,
+				recipe = n.recipe
+			}
+			
+	    -- Register the Recipes
+	    minetest.register_craft(rec)
+
+	end -- if(minetest.get_modpath(n.mod)
+end -- for i,n in ipairs(recipelist)
--- recipes.lua
+++ recipes.lua
@@ -2,55 +2,64 @@ recipelist =
         {
             
             {
+		mod = "farming",
 		type = "shapeless",
 		output = "farming:raspberries",
 		recipe = {"farming_plus:raspberry_item"}
             },
                 
             {
+		mod = "farming",
 		type = "shapeless",
 		output = "farming_plus:raspberry_item",
 		recipe = {"farming:raspberries"}
             },
 
-	   {  
+	   {
+		mod = "food_sweet",  
 		type = "shapeless",
 		output = "bushes:blueberry",
 		recipe = {"food_sweet:blueberry"}
 	   },
 	   	   
 		
-	  {  
+	  {
+		mod = "bushes",  
 		type = "shapeless",
 		output = "food_sweet:blueberry",
 		recipe = {"bushes:blueberry"}
 	   },
 
 	   {
+		mod = "mesecons_luacontroller",
 		type = "shapeless",
 		output = "mesecons_luacontroller:luacontroller0000",
 		recipe = {"mesecons_microcontroller:microcontroller0000"}
 	   },
 	   
 	   {
+		mod = "mesecons_luacontroller",
 		type = "shapeless",
 		output = "mesecons_microcontroller:microcontroller0000",
 		recipe = {"mesecons_luacontroller:luacontroller0000"}
 	   },
 
 	   {
+		mod = "moreores",
 		type = "shapeless",
 		output = "moreores:tin_lump",
 		recipe = {"default:tin_lump"}
 	   },
 	   
 	   {
+		mod = "moreores",
 		type = "shapeless",
 		output = "moreores:tin_ingot",
 		recipe = {"default:tin_ingot"}
 	   },
 
 	   {
+		mod = "moreores",
 		type = "shapeless",
 		output = "moreores:tin_block",
 		recipe = {"default:tin_block"}
